3f0f636525fac993863626a7bc0703dbe7cced48
Ousman <usman.traid@gmail.com>
2023-05-04 14:17:48 -0400

    max split year possible edited

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,7 +1,7 @@
 -- Lists all bands with Glam rock as their main style,
 -- ranked by their logevity
 
-SELECT band_name, ifnull(split, 2022)-ifnull(formed, 0) AS lifespan
+SELECT band_name, (ifnull(split, 2022)- formed) AS lifespan
 FROM metal_bands
 WHERE style LIKE '%Glam rock%'
 ORDER BY lifespan DESC;
