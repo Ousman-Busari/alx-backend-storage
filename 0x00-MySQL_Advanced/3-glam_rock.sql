e24d18feca998f5ccf02fd09f5f54502b4abf40e
Ousman <usman.traid@gmail.com>
2023-05-04 13:51:46 -0400

    present year chnaged to 2020

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,7 +1,7 @@
 -- Lists all bands with Glam rock as their main style,
 -- ranked by their logevity
 
-SELECT band_name, (IFNULL(split, 2023) - formed) AS life_span
+SELECT band_name, (IFNULL(split, 2020) - formed) AS life_span
     FROM metal_bands
     WHERE style LIKE '%Glam rock%'
     ORDER BY life_span DESC
