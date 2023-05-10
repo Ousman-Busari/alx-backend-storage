b8d20819705daefabc9474c2646ff126825af991
Ousman <usman.traid@gmail.com>
2023-05-10 18:44:58 -0400

    code refactored

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,43 +1,82 @@
 #!/usr/bin/env python3
-"""
-web
-"""
-from functools import wraps
-from typing import Callable
+# """
+# web
+# """
+# from functools import wraps
+# from typing import Callable
+# import redis
+# import requests
+
+
+# r = redis.Redis()
+# """Global redis instance"""
+
+
+# def count_url(func: Callable) -> Callable:
+#     """
+#     cache the result of method with an
+#     expiration time of 10 seconds
+#     """
+#     @wraps(func)
+#     def wrapper(url: str) -> str:
+#         """
+#         wraps method and cachees it result
+#         """
+#         r.incr(f"count:{url}")
+#         res = r.get(f"response:{url}")
+#         if res:
+#             return res.decode("utf-8")
+#         res = func(url)
+#         r.set(f"count:{url}", 0)
+#         r.setex(f"response:{url}", 10, res)
+#         return res
+#     return wrapper
+
+
+# @count_url
+# def get_page(url: str) -> str:
+#     """
+#     Track how many times a particular URL
+#     was accessed in the key "count:{url}"
+#     """
+#     res = requests.get(url)
+#     return res.text
+"""Expiring web cache Module"""
 import redis
 import requests
+from functools import wraps
+from typing import Callable
 
 
 r = redis.Redis()
-"""Global redis instance"""
+"""Module-wide Redis instance"""
 
 
-def count_url(func: Callable) -> Callable:
+def count_url(method: Callable) -> Callable:
     """
-    cache the result of method with an
-    expiration time of 10 seconds
+    A wrapper function that tracks how many times
+    a particular URL was accessed. It caches the
+    result with an expiration time of 10 seconds.
     """
-    @wraps(func)
+    @wraps(method)
     def wrapper(url: str) -> str:
-        """
-        wraps method and cachees it result
-        """
+        """Wrapper function that caches the result"""
         r.incr(f"count:{url}")
-        res = r.get(f"response:{url}")
-        if res:
-            return res.decode("utf-8")
-        res = func(url)
+        result = r.get(f"result:{url}")
+        if result:
+            return result.decode("utf-8")
+        result = method(url)
         r.set(f"count:{url}", 0)
-        r.setex(f"response:{url}", 10, res)
-        return res
+        r.setex(f"result:{url}", 10, result)
+        return result
     return wrapper
 
 
 @count_url
 def get_page(url: str) -> str:
     """
-    Track how many times a particular URL
-    was accessed in the key "count:{url}"
+    Uses the `requests` module to obtain the HTML
+    content of a particular URL and returns it.
     """
-    res = requests.get(url)
-    return res.text
+    result = requests.get(url)
+    return result.text
