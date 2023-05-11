391393ae13e2f4e74e8ed143776f501932360692
Ousman <usman.traid@gmail.com>
2023-05-11 05:18:59 -0400

    code refactored

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -12,7 +12,7 @@ r = redis.Redis()
 """Global redis instance"""
 
 
-def count_url(method: Callable) -> Callable:
+def count_and_cache_url(method: Callable) -> Callable:
     """
     cache the result of method with an
     expiration time of 10 seconds
@@ -22,13 +22,18 @@ def count_url(method: Callable) -> Callable:
         """
         wraps method and cachees it result
         """
-        r.incrby(f"count:{{{url}}}", 1)
-        r.expire(f"count:{{{url}}}", 10)
-        return method(url)
+        r.incr(f"count:{{{url}}}")
+        res = r.get(f"result:{{{url}}}")
+        if res:
+            return res.decode("utf-8")
+        res = method(url)
+        r.set(f"count:{{{url}}}", 1)
+        r.setex(f"result:{{{url}}}", 10, res)
+        return res
     return wrapper
 
 
-@count_url
+@count_and_cache_url
 def get_page(url: str) -> str:
     """
     Track how many times a particular URL
